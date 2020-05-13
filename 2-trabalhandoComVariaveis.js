ab0bcb9eb7552e2d224e97a8fa8172f6a1c4f919
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-13 17:49:23 -0300

    aula 2 completa

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,11 @@
+// Console.log("Trabalhando com variáveis");
+console.log("Trabalhando com variáveis");
+// JS é case sensitive
+
+const idade = 29;
+const nome = "Jonilson";
+
+// não fazer
+// mes = "Janeiro";
+
+
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,13 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+let primeiroprimeiroNome = "Jonilson";
+const sobrenome = "Silva";
+
+// console.log(primeiroNome + " " + sobreprimeiroNome);
+console.log(primeiroNome, sobreprimeiroNome);
+console.log(`Meu primeiroNome é ${primeiroNome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
