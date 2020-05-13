ea6cec0070f0b6b971da2700fc5526b514172699
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-13 18:09:39 -0300

    aula 3 completa

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,13 +1,20 @@
 console.log("Trabalhando com atribuição de variáveis");
 
-const idade = 29;
-let primeiroprimeiroNome = "Jonilson";
+let primeiroNome = "Jonilson";
 const sobrenome = "Silva";
 
 // console.log(primeiroNome + " " + sobreprimeiroNome);
-console.log(primeiroNome, sobreprimeiroNome);
+console.log(primeiroNome, sobrenome);
 console.log(`Meu primeiroNome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
 
 const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
+
+let idade;  //declarando variável
+idade = 26; //atribuindo valor
+idade = idade+1;
+console.log(idade);
 
